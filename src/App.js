65b26a25ad64ddc0cbe517840799b00694260444
Daisy Barrette <daisybarrettecodes@gmail.com>
2023-10-11 12:02:59 -0400

    WIP - Track attributes in state, partial increment controls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 
 function App() {
     const [num, setNum] = useState(0);
+    const [attributeValues, setAttributeValues] = useState(ATTRIBUTE_LIST.map((attribute) => ({ [attribute]: 0 })));
+
+    function handleIncrementAttribute(attribute) {
+        console.log('incrementing attribute: ', attribute);
+        const updatedAttributeValues = [...attributeValues];
+
+        const attributeIndex = updatedAttributeValues.findIndex((element) => Object.keys(element).includes(attribute));
+
+        const attributeToUpdate = updatedAttributeValues[attributeIndex];
+
+        console.log('attributeToUpdate', attributeToUpdate);
+
+        updatedAttributeValues[attribute] = updatedAttributeValues[attribute] + 1;
+
+        setAttributeValues(updatedAttributeValues);
+    }
+
+    console.log('attributeValues', attributeValues);
+
     return (
         <div className='App'>
             <header className='App-header'>
@@ -15,6 +34,16 @@ function App() {
                     {num}
                     <button>+</button>
                     <button>-</button>
+                </div>
+                <div className='App-column'>
+                    <h2>Attributes</h2>
+                    {ATTRIBUTE_LIST.map((attribute) => (
+                        <div key={attribute}>
+                            {attribute}: {num}
+                            <button onClick={() => handleIncrementAttribute(attribute)}>+</button>
+                            <button>-</button>
+                        </div>
+                    ))}
                 </div>
             </section>
         </div>
